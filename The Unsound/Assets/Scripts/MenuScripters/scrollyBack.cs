15b5da798fa92fdaa35959c25fda4ec11537873d
denkiapple <uTorner@Sherlock-2.local>
2017-01-21 14:24:01 -0600

    Menu Créditos
    Los Créditos ya están online

---
+++ The Unsound/Assets/Scripts/MenuScripters/scrollyBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollyBack : MonoBehaviour {
+
+	public float scrollSpeed = 0.5f;
+	public float offset;
+	public float rotate;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset+= (Time.deltaTime /* *scrollSpeed */)/10.0f;
+		GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset,0));
+	}
+}
