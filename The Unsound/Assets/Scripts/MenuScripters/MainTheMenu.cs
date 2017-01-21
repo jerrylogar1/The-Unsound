4a95a5317e0c6817881a8c8fff6efcf3b5432b37
denkiapple <uTorner@Sherlock-2.local>
2017-01-21 08:28:15 -0600

    Menues Principal y Settings
    Menues y navegación entre main menu, settings menú y quitar

---
+++ The Unsound/Assets/Scripts/MenuScripters/MainTheMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class MainTheMenu : MonoBehaviour {
+
+	public bool isStart;
+	public bool isSettings;
+	public bool isEnd;
+	public bool isMain;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseUp () {
+		if (isStart) {
+			SceneManager.LoadScene("GameScene");
+			GetComponent<Renderer> ().material.color = Color.cyan;
+		}
+
+		if (isSettings) {
+			SceneManager.LoadScene ("SettingsMenu");
+			GetComponent<Renderer> ().material.color = Color.cyan;
+		}
+
+		if (isMain) {
+			SceneManager.LoadScene ("Menu");
+			GetComponent<Renderer> ().material.color = Color.cyan;
+		}
+
+		if (isEnd) {
+			Application.Quit();
+			GetComponent<Renderer> ().material.color = Color.cyan;
+		}
+	}
+}
